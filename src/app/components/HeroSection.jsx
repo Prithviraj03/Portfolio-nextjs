50b18b836211922f65e0f68e95978f842e4931f2
Prithviraj Pawar <prithviraj_09@yahoo.com>
2024-09-01 20:02:22 -0500

    changed element <link> to <a>

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -37,7 +37,9 @@ const HeroSection = () => {
             />
           </h1>
           <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
-            I'm a Software Developer skilled in Java, Python, and front-end frameworks like ReactJS and Angular. I specialize in creating scalable solutions, developing RESTful APIs, and deploying applications to cloud platforms like Microsoft Azure. I’m passionate about using my expertise in software development and CI/CD to drive impactful projects.
+            I'm a Software Developer skilled in Java, Python, and front-end frameworks like ReactJS and Angular.
+            I specialize in creating scalable solutions, developing RESTful APIs, and deploying applications to cloud platforms like Microsoft Azure.
+            I’m passionate about using my expertise in software development and CI/CD to drive impactful projects.
           </p>
           <div>
             <a
